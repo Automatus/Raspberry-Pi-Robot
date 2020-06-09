976b959c923808e32cb6f1697bceefafe50719e7
Automatus <57252435+Automatus@users.noreply.github.com>
2020-06-09 12:34:07 +0200

    Update Robot.py

--- Robot.py
+++ Robot.py
@@ -1,1 +1,19 @@
-test
+from gpiozero import Motor
+from gpiozero import DistanceSensor
+from time import sleep
+
+motor = Motor(forward=12, backward=18)
+motorb = Motor(forward=19, backward=13)
+sensor = DistanceSensor(23, 24)
+
+while True:
+    if sensor.distance < 0.3:
+        motor.forward()
+        motorb.backward()
+        sleep(0.2)
+    else:
+        motorb.forward()
+        motor.forward()
+        sleep(0.2)
+
+  # https://gpiozero.readthedocs.io/en/stable/recipes.html#distance-sensor
